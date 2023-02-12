ac84de723894b2fa8bcf8d64cbd6edcba609d256
sFiFty <alexandr.rudin.my@gmail.com>
2023-02-12 15:19:29 +0100

    image improvements

--- src/commands/tools/image.js
+++ src/commands/tools/image.js
@@ -24,33 +24,49 @@ module.exports = {
           {name: "MD(512x512)", value: "512x512"},
           {name: "LG(1024x1024)", value: "1024x1024"}
         )
+      )
+    .addStringOption(option =>
+      option.setName('quantity')
+        .setDescription('Images quantity. Default: 1')
+        .setChoices(
+          {name: "1", value: "1"}, 
+          {name: "2", value: "2"},
+          {name: "3", value: "3"},
+          {name: "4", value: "4"}
+        )
       ),
   async execute(interaction) {
     const optionDescription = interaction.options.get('description');
     const optionSize = interaction.options.get('size');
+    const optionNumber = interaction.options.get('quantity');
 
     await interaction.deferReply({
       fetchReply: true
     })
 
     const size = optionSize ? optionSize.value : "256x256";
+    const number = optionNumber ? Number(optionNumber.value) : 1;
 
     const response = await openai.createImage({
       prompt: optionDescription.value,
-      n: 1,
+      n: number,
       size: size,
     });
 
-    image_url = response.data.data[0].url;
-
     const embed = new EmbedBuilder()
-      .setTitle(`Description: ${optionDescription.value}\nSize: ${size}`)
-      .setDescription(image_url);
+      .setTitle(`Size: ${size}\nNumber: ${size}`)
+      .setDescription(optionDescription.value)
 
+      
+    images = response.data.data;
+      
+    const imagesEmbeds = images.map(image => {
+      return new EmbedBuilder().setImage(image.url);
+    })
+    
     await interaction.editReply({
       ephemeral: true,
-      embeds: [embed]
+      embeds: [embed, ...imagesEmbeds]
     })
   }
-    
 } 
