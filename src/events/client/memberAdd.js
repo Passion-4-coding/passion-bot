167d6b83ed75b13ff539bf09ec61956288562bce
sFiFty <alexandr.rudin.my@gmail.com>
2023-02-18 19:32:33 +0100

    add logs

--- src/events/client/memberAdd.js
+++ src/events/client/memberAdd.js
@@ -4,6 +4,7 @@ module.exports = {
   name: Events.GuildMemberAdd,
   once: false,
   async execute(member, client) {
+    console.log(member);
     const role = member.guild.roles.cache.find(role => role.name === "Trainee");
     member.roles.add(role);
   }
