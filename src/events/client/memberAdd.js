02e06cd246d3e8febce746fce16ab0dbb35b357f
sFiFty <alexandr.rudin.my@gmail.com>
2023-02-21 17:22:08 +0100

    add logs for test

--- src/events/client/memberAdd.js
+++ src/events/client/memberAdd.js
@@ -4,9 +4,9 @@ module.exports = {
   name: Events.GuildMemberAdd,
   once: false,
   async execute(member) {
-    console.log(member);
+    console.log("GuildMemberAdd member", member);
     const role = member.guild.roles.cache.get("850393978926006294");
-    console.log(role);
+    console.log("GuildMemberAdd role", role);
     member.roles.add(role).catch(console.error);
   }
 }
--- src/events/client/message.js
+++ src/events/client/message.js
@@ -4,5 +4,6 @@ module.exports = {
   name: Events.MessageCreate,
   once: false,
   async execute(interaction, client) {
+    console.log("MessageCreate", member);
   }
 }
