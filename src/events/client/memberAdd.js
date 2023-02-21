e4254190c947786a596e2c86ad2198e6c07f6d0d
sFiFty <alexandr.rudin.my@gmail.com>
2023-02-21 17:19:56 +0100

    add logs

--- src/events/client/memberAdd.js
+++ src/events/client/memberAdd.js
@@ -4,7 +4,9 @@ module.exports = {
   name: Events.GuildMemberAdd,
   once: false,
   async execute(member) {
+    console.log(member);
     const role = member.guild.roles.cache.get("850393978926006294");
+    console.log(role);
     member.roles.add(role).catch(console.error);
   }
 }
