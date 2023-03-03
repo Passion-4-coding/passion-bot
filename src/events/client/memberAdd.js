08d5565f385c2c8c84323d30cc1181216a157dc3
sFiFty <alexandr.rudin.my@gmail.com>
2023-03-03 18:36:15 +0100

    fix member add

--- src/events/client/memberAdd.js
+++ src/events/client/memberAdd.js
@@ -8,22 +8,23 @@ module.exports = {
     const role = member.guild.roles.cache.get("850393978926006294");
     member.roles.add(role).catch(console.error);
 
-    // const randomLanguage = languages[Math.floor(Math.random()*languages.length)];
-    // const randomMessage = welcome.messages[randomLanguage][Math.floor(Math.random()*welcome.messages[randomLanguage].length)];
-    // const otherLanguages = languages.filter(l => l !== randomLanguage);
-    // const languageRole = interaction.guild.roles.cache.get(languageRoles[randomLanguage]);
-    // interaction.member.roles.add(languageRole).catch(console.error);
+    const channel = client.channels.cache.get("876024439245000746");
+    const randomLanguage = languages[Math.floor(Math.random()*languages.length)];
+    const randomMessage = welcome.messages[randomLanguage][Math.floor(Math.random()*welcome.messages[randomLanguage].length)];
+    const otherLanguages = languages.filter(l => l !== randomLanguage);
+    const languageRole = interaction.guild.roles.cache.get(languageRoles[randomLanguage]);
+    interaction.member.roles.add(languageRole).catch(console.error);
 
-    // const buttons = new ActionRowBuilder()
-    // for(const language of otherLanguages) {
-    //   buttons.addComponents(
-    //     new ButtonBuilder()
-    //       .setCustomId(`${languageRoles[randomLanguage]}:${language}:${interaction.member.id}`)
-    //       .setLabel(welcome.buttons[language])
-    //       .setStyle(ButtonStyle.Primary),
-    //   );
-    // }
+    const buttons = new ActionRowBuilder()
+    for(const language of otherLanguages) {
+      buttons.addComponents(
+        new ButtonBuilder()
+          .setCustomId(`${languageRoles[randomLanguage]}:${language}:${interaction.member.id}`)
+          .setLabel(welcome.buttons[language])
+          .setStyle(ButtonStyle.Primary),
+      );
+    }
   
-    // await interaction.reply({ content: randomMessage, components: [buttons] });
+    await channel.send({ content: `${member}\n${randomMessage}`, components: [buttons] });
   }
 }
