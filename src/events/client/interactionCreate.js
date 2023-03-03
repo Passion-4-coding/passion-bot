4b887df65a2c3a59a36c5941378447e2e5a3760b
sFiFty <alexandr.rudin.my@gmail.com>
2023-03-03 18:19:21 +0100

    disable button interaction

--- src/events/client/interactionCreate.js
+++ src/events/client/interactionCreate.js
@@ -4,19 +4,19 @@ const { Events } = require("discord.js");
 module.exports = {
   name: Events.InteractionCreate,
   async execute(interaction, client) {
-    if (interaction.isButton) {
-      const ids = interaction.customId.split(":");
-      const currentLanguage = ids[1];
-      const memberId = ids[2];
-      if (memberId !== interaction.member.id) return;
-      const randomMessage = welcome.messages[currentLanguage][Math.floor(Math.random()*welcome.messages[currentLanguage].length)];
-      for(const roleId of Object.values(languageRoles)) {
-        interaction.member.roles.remove(roleId);
-      }
-      interaction.member.roles.add(languageRoles[currentLanguage]);
-      interaction.update(`${randomMessage}\n\n${welcome.replies[currentLanguage]}`);
-      return;
-    }
+    // if (interaction.isButton) {
+    //   const ids = interaction.customId.split(":");
+    //   const currentLanguage = ids[1];
+    //   const memberId = ids[2];
+    //   if (memberId !== interaction.member.id) return;
+    //   const randomMessage = welcome.messages[currentLanguage][Math.floor(Math.random()*welcome.messages[currentLanguage].length)];
+    //   for(const roleId of Object.values(languageRoles)) {
+    //     interaction.member.roles.remove(roleId);
+    //   }
+    //   interaction.member.roles.add(languageRoles[currentLanguage]);
+    //   interaction.update(`${randomMessage}\n\n${welcome.replies[currentLanguage]}`);
+    //   return;
+    // }
     if (interaction.isChatInputCommand()) {
       const { commands } = client;
       const { commandName } = interaction;
