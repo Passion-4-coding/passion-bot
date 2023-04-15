f35faa4ea16b87515edd228dcc8d520344fd286f
sFiFty <alexandr.rudin.my@gmail.com>
2023-04-15 10:03:40 +0200

    move buttons back

--- src/events/client/memberAdd.js
+++ src/events/client/memberAdd.js
@@ -9,23 +9,22 @@ module.exports = {
   async execute(member) {
     const role = member.guild.roles.cache.get(roles.trainee);
     member.roles.add(role).catch(console.error);
-    member.guild.roles.cache.forEach(role => console.log(role.name, role.id))
     const channel = member.guild.channels.cache.get(channels.reception);
     const randomLanguage = languages[Math.floor(Math.random()*languages.length)];
-    const randomMessage = welcome.messages.en[Math.floor(Math.random()*welcome.messages.en.length)];
-    //const otherLanguages = languages.filter(l => l !== randomLanguage);
-    //const languageRole = member.guild.roles.cache.get(roles[randomLanguage]);
-    //member.roles.add(languageRole).catch(console.error);
+    const randomMessage = welcome.messages[randomLanguage][Math.floor(Math.random()*welcome.messages[randomLanguage].length)];
+    const otherLanguages = languages.filter(l => l !== randomLanguage);
+    const languageRole = member.guild.roles.cache.get(roles[randomLanguage]);
+    member.roles.add(languageRole).catch(console.error);
 
-    // const buttons = new ActionRowBuilder()
-    // for(const language of otherLanguages) {
-    //   buttons.addComponents(
-    //     new ButtonBuilder()
-    //       .setCustomId(`${roles[randomLanguage]}:${language}:${member.id}`)
-    //       .setLabel(welcome.buttons[language])
-    //       .setStyle(ButtonStyle.Primary),
-    //   );
-    // }
-    await channel.send({ content: `${member}\n${randomMessage}` });
+    const buttons = new ActionRowBuilder()
+    for(const language of otherLanguages) {
+      buttons.addComponents(
+        new ButtonBuilder()
+          .setCustomId(`${roles[randomLanguage]}:${language}:${member.id}`)
+          .setLabel(welcome.buttons[language])
+          .setStyle(ButtonStyle.Primary),
+      );
+    }
+    await channel.send({ content: `${member}\n${randomMessage}`, components: [buttons] });
   }
 }
