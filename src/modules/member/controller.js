2b2c9799eb45475e689650f75e8448778a1dc284
sFiFty <alexandr.rudin.my@gmail.com>
2023-05-13 15:14:03 +0200

    fix bot indication

--- src/modules/member/controller.js
+++ src/modules/member/controller.js
@@ -26,7 +26,7 @@ const _addMember = (discordMember) => {
     discordId: discordMember.id,
     username: discordMember.username,
     isActive: true,
-    isBot: discordMember.bot,
+    isBot: discordMember.user ? discordMember.user.bot : false,
     isTest: false,
   }
   return addMember(member);
