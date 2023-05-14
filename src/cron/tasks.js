9b826a4bbcaa7094fb1d299660845fb3637a95a6
sFiFty <alexandr.rudin.my@gmail.com>
2023-05-14 19:11:45 +0200

    add stat task

--- src/cron/tasks.js
+++ src/cron/tasks.js
@@ -1,11 +1,19 @@
 const cron = require('node-cron');
 const { channels } = require("../constants");
 const { getKarmaLeaderBoard } = require('../modules/karma');
+const { getPastDayStats } = require('../modules/stats');
 
 const runTasks = (client) => {
   cron.schedule('0 16 * * *', async () => {
     const channel = client.channels.cache.get(channels.coffee);
     const embed = await getKarmaLeaderBoard();
+    channel.send({ embeds: [embed] });
+  }, {
+    timezone: 'Europe/Warsaw'
+  });
+  cron.schedule('0 12 * * *', async () => {
+    const channel = client.channels.cache.get(channels.coffee);
+    const embed = await getPastDayStats();
     channel.send({ embeds: [embed] });
   }, {
     timezone: 'Europe/Warsaw'
